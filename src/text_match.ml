6f65ba1ee596e400dfa2bdf0d438cdc48dc1d996
derui <derutakayu@gmail.com>
2015-01-18 16:20:05 +0900

    Decoration for text apply only matched string.

--- src/text_match.ml
+++ src/text_match.ml
@@ -6,8 +6,7 @@ module ReIntf = CamomileLibraryDefault.Camomile.UReStr
 module Re = ReIntf.Make (UTF8)
 
 let matched_style = {LTerm_style.none with
-  LTerm_style.bold = Some true;
-  reverse = Some true;
+  LTerm_style.reverse = Some true;
 }
 
 let decorate_to_match text candidate =
@@ -16,8 +15,8 @@ let decorate_to_match text candidate =
   match Re.regexp_match ~sem:`First regexp (C.text candidate) 0 with
   | None -> candidate
   | Some (texts) -> begin match texts with
-    | [|Some first;_|] ->
-       let range = C.index_range_of_text candidate in
+    | [|Some _;Some first|] ->
+       let range = Re.SubText.length first |> List.range ~stop:`exclusive 0 in
        let first = Re.SubText.first first |> Re.SubText.ur_index_of first in
        let f memo index = (index + first, matched_style) :: memo in
        let styles = List.fold ~init:[] ~f range in
