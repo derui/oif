ab668aeff676168203a5da410d479f381ae99a66
derui <derutakayu@gmail.com>
2015-03-14 18:02:26 +0900

    Refactor source.

--- src/main.ml
+++ src/main.ml
@@ -12,8 +12,8 @@ let load_lines channel =
   catch (fun () ->
     let rec read_lines channel lines =
       catch (fun () ->
-             Lwt_io.read_line channel >>= fun line ->
-             read_lines channel (line :: lines)
+        Lwt_io.read_line channel >>= fun line ->
+        read_lines channel (line :: lines)
       ) (fun _ -> return lines)
     in
     read_lines channel []
@@ -29,55 +29,42 @@ let process_when_tty window is_a_tty =
     LTerm.set_io ~incoming_channel:in_chan window >>= fun () -> return lines
   end
 
+let make_info lines = 
+  let open Lwt in
+  let lines = List.rev lines in
+  return {Types.Info.empty with
+    Types.Info.candidates = List.map lines ~f:(Types.Candidate.make) |> List.map ~f:Option.some;
+    lines;
+  }
+
+let event_handler window term info () = 
+  let module I = Types.Info in
+  let render info' =
+    Renderer.render window info'.Types.Info.candidates info'.Types.Info.selection in
+  let text = Zed_edit.text term#edit |> Zed_rope.to_string  in
+  let info = Filter.filter info text in
+  let info = React.S.value term#selection |> Filter.update_selection info in
+  render info
+
 let () =
   let open Lwt in
   let window = LTerm.create Lwt_unix.stdin Lwt_io.stdin Lwt_unix.stdout Lwt_io.stdout in
-  LTerm_read_line.bind [{LTerm_key.code = LTerm_key.Char(UChar.of_char 'n');
-                         control = true; meta = false; shift = false}]
-                       [LTerm_read_line.Complete_bar_next];
-  LTerm_read_line.bind [{LTerm_key.code = LTerm_key.Char(UChar.of_char 'p');
-                         control = true; meta = false; shift = false}]
-                       [LTerm_read_line.Complete_bar_prev];
 
   begin
-    let info = Types.Info.empty in
-    let monad =
-      let module I = Types.Info in
-      window >>= fun window -> return (LTerm.is_a_tty window) >>= fun is_a_tty ->
-      process_when_tty window is_a_tty >>= fun lines ->
-      let lines = List.rev lines in
-      info.I.lines <- lines;
-      info.I.candidates <- List.map lines ~f:(Types.Candidate.make) |> List.map ~f:Option.some;
-      let info = {Types.Info.empty with
-                   Types.Info.candidates = List.map lines ~f:(Types.Candidate.make) |> List.map ~f:Option.some;
-                 lines;
-              } in
-      let info_e, info_send = React.E.create () in
-      return () >>= fun () ->
+    let monad = window >>= fun window -> return (LTerm.is_a_tty window) >>= fun is_a_tty ->
+      process_when_tty window is_a_tty >>= make_info >>= fun info ->
       LTerm.clear_screen window >>= fun () ->
-      let term = new Read_line.read_line window [] 0 in
+      let term = new Read_line.read_line window [] 0 info in
       let module S = LTerm_style in
       Renderer.render window info.Types.Info.candidates None >>= fun () ->
       LTerm.goto window {LTerm_geom.row = 0;col = 0} >>= fun () ->
 
-      let render info' =
-        Renderer.render window info'.Types.Info.candidates info'.Types.Info.selection in
-
       let select = React.E.select 
-                     [React.E.stamp (React.S.changes term#selection) () ;
-                      React.E.stamp (Zed_edit.changes term#edit) ()
-                     ] in
+        [React.E.stamp (React.S.changes term#selection) () ;
+         React.E.stamp (Zed_edit.changes term#edit) ()
+        ] in
       Lwt_react.E.keep select;
-      Lwt_react.E.map_s
-        (fun () ->
-         let module I = Types.Info in
-         let text = Zed_edit.text term#edit |> Zed_rope.to_string  in
-         let info = Filter.filter info text in
-         let info = React.S.value term#selection |> Filter.update_selection info in
-         render info
-        ) select |> ignore;
-
-      Lwt_react.E.map_s render info_e |> ignore; term#run
+      Lwt_react.E.map_s (event_handler window term info) select |> return >>= fun _ -> term#run
     in 
     let v = Lwt_main.run monad in
     Printf.printf "%s" v
--- src/read_line.ml
+++ src/read_line.ml
@@ -1,7 +1,7 @@
 open CamomileLibrary
 open Core.Std
 
-class read_line ~term ~history ~exit_code =
+class read_line ~term ~history ~exit_code info =
   let selection_s, set_selection = React.S.create ~eq:(fun _ _ -> false) (None : Types.direction option) in
 object(self)
   inherit LTerm_read_line.read_line ()
@@ -20,7 +20,17 @@ object(self)
     match actions with
     | [] -> super#exec []
     | action :: rest -> begin match action with
-      | LTerm_read_line.Accept -> return (Zed_edit.text self#edit |> Zed_rope.to_string)
+      | LTerm_read_line.Accept -> begin
+         let module I = Types.Info in
+         let module C = Types.Candidate in
+         let candidates = List.map ~f:Option.to_list info.I.candidates |> List.concat |> List.map ~f:C.text in
+         match info.I.selection with
+         | None -> return ""
+         | Some (row, _) ->
+            match List.nth candidates row with
+            | None -> return ""
+            | Some selection -> return selection
+      end
       | LTerm_read_line.Complete -> return (Zed_edit.text self#edit |> Zed_rope.to_string)
       | LTerm_read_line.Complete_bar_next ->
          self#set_selection (Some Types.Next);
@@ -36,5 +46,17 @@ object(self)
   self#set_prompt signal;
 
   Zed_macro.add self#macro LTerm_read_line.Accept;
+  let module K = LTerm_key in
+  LTerm_read_line.bind [{LTerm_key.code = LTerm_key.Char(UChar.of_char 'n');
+                         control = true; meta = false; shift = false}]
+    [LTerm_read_line.Complete_bar_next];
+  LTerm_read_line.bind [{LTerm_key.code = LTerm_key.Char(UChar.of_char 'p');
+                         control = true; meta = false; shift = false}]
+    [LTerm_read_line.Complete_bar_prev];
+  LTerm_read_line.bind [{K.control = true;
+                         meta = false;
+                         shift = false;
+                         code = K.Char (UChar.of_char ' ')}]
+    [LTerm_read_line.Complete]
 
 end
